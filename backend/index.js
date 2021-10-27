b2d0a360d8e2e5f621e4764830109cdf072d9911
Xander Berkein <xander.berkein@gmail.com>
2021-10-27 09:19:22 +0200

    Add cors exceptions + extra packages

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
+const loremIpsum =
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam obcaecati laudantium recusandae, debitis eum voluptatem ad, illo voluptatibus temporibus odio provident. Laboriosam accusamus necessitatibus tenetur praesentium ullam voluptates nulla reprehenderit? 🤣';
+const date = '2021-10-26T17:09:00.000Z';
+
 function App() {
   return (
     <div>Hi Odisee</div>
