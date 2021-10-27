3f5c4a6c216cd130e3c22927b494b0aa6297446b
Xander Berkein <xander.berkein@gmail.com>
2021-10-27 17:13:43 +0200

    Move socket io listeners to useEffect and add cleanup effects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,28 +35,58 @@ function App() {
     fetchOditweets();
   }, []);
 
+  /**
+   * I made a small (but impactful) mistake in the classroom:
+   * the socket.io listeners were put directly in the root of the App component.
+   * This means: if the App rerenders (because a tweet or a like gets added), socket.on()
+   * would be called every time.
+   * Thus, the more updates that came in, the more listeners would start actively listening
+   * for the same thing. This also explains the multiplying logs in the console.log every time
+   * an update came in
+   *
+   * To fix this:
+   * Move the listeners to a useEffect like below
+   * We want the useEffect to run everytime the "oditweets" state updates, to make sure that
+   * "addOditweet" and "updateOditweet" always have the latest data to work with
+   * See for yourself: if you remove "oditweets" from the dependency array, this code will
+   * not work as expected.
+   * Instead, everytime the useEffect triggers -> re-initalize the socket listeners
+   * BUT -> add a cleanup function. Everytime this useEffect trigger, we'll also close the
+   * previous listeners
+   */
+  useEffect(() => {
+    socket.on('oditweet', addOditweet);
+    socket.on('likes-server', updateOditweet);
+
+    return function cleanup() {
+      socket.off('oditweet');
+      socket.off('likes-server');
+    };
+  }, [oditweets]);
+
   const sendMessageToServer = (message) => {
     socket.emit('oditweet', { content: message, name: 'Xander', date: formatISO(new Date()) });
   };
-
-  socket.on('oditweet', (oditweet) => {
-    setOditweets([...oditweets, oditweet]);
-  });
 
   const likeOditweet = (id) => {
     socket.emit('likes', { id });
   };
 
-  socket.on('likes-server', ({ id, likes }) => {
-    const oditweetsCopy = [...oditweets];
+  const addOditweet = (oditweet) => {
+    console.log('add oditweet', oditweet);
+    setOditweets([...oditweets, oditweet]);
+  };
 
-    const oditweetToUpdateIndex = oditweetsCopy.findIndex((o) => o.id === id);
-    const oditweetToUpdateCopy = { ...oditweetsCopy[oditweetToUpdateIndex] };
+  const updateOditweet = ({ id, likes }) => {
+    console.log('add likes to oditweet', id, likes);
+    const oditweetToUpdateIndex = oditweets.findIndex((o) => o.id === id);
+    const oditweetToUpdateCopy = { ...oditweets[oditweetToUpdateIndex] };
 
     oditweetToUpdateCopy.likes = likes;
 
-    setOditweets(update(oditweetToUpdateIndex, oditweetToUpdateCopy, oditweetsCopy));
-  });
+    // Ramda's "update" return a new array, to make sure we don't break the state's immutability
+    setOditweets(update(oditweetToUpdateIndex, oditweetToUpdateCopy, oditweets));
+  };
 
   return (
     <div className="min-w-screen min-h-screen bg-gray-200 flex-row justify-center items-center px-5 py-5">
